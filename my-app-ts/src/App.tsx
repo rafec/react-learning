76afc97f00ccd4655b2569b8457abef6649fc884
rafec <rafael.brizuena@gmail.com>
2023-08-01 20:33:38 -0400

    created default app and tested the card component

---
+++ my-app-ts/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Card } from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      Hello world!
+      <div>
+        <h1>Card</h1>
+        <p>Parágrafo</p>
+      </div>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
