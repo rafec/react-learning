a1f519ad58ff21346f2e81cfd4286ba792cf32c0
rafec <rafael.brizuena@gmail.com>
2023-08-01 21:35:04 -0400

    feat: created component Layout with components header and footer inside

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import "./App.css";
 import { Card } from "./components/Card";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="App">
-        Hello world!
-        <Card id={1} paragraph="Typescript" details="TS for back-end" />
-        <Card id={2} paragraph="HTML" details="HTML for front-end" />
-        <Card id={3} paragraph="SQL" details="SQL for database" />
-      </div>
-      <div>Another div</div>
-      <Footer />
-    </>
+    <Layout>
+      Hello world!
+      <Card id={1} paragraph="Typescript" details="TS for back-end" />
+      <Card id={2} paragraph="HTML" details="HTML for front-end" />
+      <Card id={3} paragraph="SQL" details="SQL for database" />
+    </Layout>
   );
 }
 
