12c1316124692852532ea44fd3487f32a6c93add
rafec <rafael.brizuena@gmail.com>
2023-08-01 21:10:38 -0400

    feat: created header and footer components

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <div className="App">
         Hello world!
         <Card id={1} paragraph="Typescript" details="TS for back-end" />
@@ -12,6 +15,7 @@ function App() {
         <Card id={3} paragraph="SQL" details="SQL for database" />
       </div>
       <div>Another div</div>
+      <Footer />
     </>
   );
 }
