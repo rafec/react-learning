8b1e0063be14a281eeb95d264152c11757626ba1
rafec <rafael.brizuena@gmail.com>
2023-08-01 21:05:29 -0400

    feat: using props drilling to pass properties into components

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -7,9 +7,9 @@ function App() {
     <>
       <div className="App">
         Hello world!
-        <Card />
-        <Card />
-        <Card />
+        <Card id={1} paragraph="Typescript" details="TS for back-end" />
+        <Card id={2} paragraph="HTML" details="HTML for front-end" />
+        <Card id={3} paragraph="SQL" details="SQL for database" />
       </div>
       <div>Another div</div>
     </>
