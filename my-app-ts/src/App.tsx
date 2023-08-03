a8337a35f91a0f6cce02af5ca12d905f358688e1
rafec <rafael.brizuena@gmail.com>
2023-08-02 20:09:44 -0400

    feat: created login inputs for the main page in app.tsx

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -6,10 +6,12 @@ import { Layout } from "./components/Layout";
 function App() {
   return (
     <Layout>
-      Hello world!
-      <Card id={1} paragraph="Typescript" details="TS for back-end" />
-      <Card id={2} paragraph="HTML" details="HTML for front-end" />
-      <Card id={3} paragraph="SQL" details="SQL for database" />
+      <h1>Login</h1>
+      <label htmlFor="emailInput">Email:</label>
+      <input type="email" id="emailInput" />
+      <label htmlFor="passwordInput">Password:</label>
+      <input type="password" id="passwordInput" />
+      <button>Login</button>
     </Layout>
   );
 }
--- my-app-ts/src/components/Header.tsx
+++ my-app-ts/src/components/Header.tsx
@@ -1,3 +1,3 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return <div>Dio Bank</div>;
 };
