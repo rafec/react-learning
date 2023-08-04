d89fac0c73287e1e48a28022a34b4588ef63ea94
rafec <rafael.brizuena@gmail.com>
2023-08-03 20:34:30 -0400

    feat: structuring and stylizing login page with Chakra UI

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,13 +1,22 @@
-import { Center, ChakraProvider, Input } from "@chakra-ui/react";
+import { Box, Button, Center, ChakraProvider, Input } from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
-      <Center>
-        <h1>Login</h1>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-      </Center>
+      <Box minHeight="100vh" backgroundColor="#7f28b5" padding="25px">
+        <Box backgroundColor="#FFFFFF" borderRadius="25px" padding="15px">
+          <Center>
+            <h1>Login</h1>
+          </Center>
+          <Input placeholder="Email" />
+          <Input placeholder="Password" />
+          <Center>
+            <Button colorScheme="teal" size="sm" width="100%" marginTop="5px">
+              LOGIN
+            </Button>
+          </Center>
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
