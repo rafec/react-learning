96c87fd033d8f274798d128211c60cfcece5c182
rafec <rafael.brizuena@gmail.com>
2023-11-04 12:54:55 -0400

    feat: added area chart

--- dashboard-react-syncfusion/src/components/Charts/SparkLine.jsx
+++ dashboard-react-syncfusion/src/components/Charts/SparkLine.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import {
-  SparklineComponent,
-  Inject,
-  SparklineTooltip,
+	SparklineComponent,
+	Inject,
+	SparklineTooltip,
 } from "@syncfusion/ej2-react-charts";
 
 const SparkLine = ({ id, height, widht, color, data, type, currentColor }) => {
-  return (
-    <SparklineComponent
-      id={id}
-      height={height}
-      width={widht}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      dataSource={data}
-      xName="x"
-      yName="y"
-      type={type}
-      tooltipSettings={{
-        visible: true,
-        format: "${x} : data ${y}",
-        trackLineSettings: { visible: true },
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  );
+	return (
+		<SparklineComponent
+			id={id}
+			height={height}
+			width={widht}
+			lineWidth={1}
+			valueType="Numeric"
+			fill={color}
+			border={{ color: currentColor, width: 2 }}
+			dataSource={data}
+			xName="x"
+			yName="y"
+			type={type}
+			tooltipSettings={{
+				visible: true,
+				format: "${x} : data ${y}",
+				trackLineSettings: { visible: true },
+			}}
+		>
+			<Inject services={[SparklineTooltip]} />
+		</SparklineComponent>
+	);
 };
 
 export default SparkLine;
