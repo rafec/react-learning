7e59fc6f29acde47be0892a3ae741eeddaeab678
rafec <rafael.brizuena@gmail.com>
2023-11-04 10:52:35 -0400

    fix: fixed alignment of divs in Color Picker page

--- dashboard-react-syncfusion/src/pages/ColorPicker.jsx
+++ dashboard-react-syncfusion/src/pages/ColorPicker.jsx
@@ -15,26 +15,28 @@ const ColorPicker = () => {
 			<div className="text-center">
 				<div id="preview" />
 				<div className="flex justify-center items-center gap-20 flex-wrap">
-					<p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
-					<ColorPickerComponent
-						id="inline-pallete"
-						mode="Palette"
-						modeSwitcher={false}
-						inline
-						showButtons={false}
-						change={change}
-					/>
-				</div>
-				<div className="flex justify-center items-center gap-20 flex-wrap">
-					<p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
-					<ColorPickerComponent
-						id="inline-pallete"
-						mode="Picker"
-						modeSwitcher={false}
-						inline
-						showButtons={false}
-						change={change}
-					/>
+					<div>
+						<p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+						<ColorPickerComponent
+							id="inline-pallete"
+							mode="Palette"
+							modeSwitcher={false}
+							inline
+							showButtons={false}
+							change={change}
+						/>
+					</div>
+					<div>
+						<p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+						<ColorPickerComponent
+							id="inline-pallete"
+							mode="Picker"
+							modeSwitcher={false}
+							inline
+							showButtons={false}
+							change={change}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
