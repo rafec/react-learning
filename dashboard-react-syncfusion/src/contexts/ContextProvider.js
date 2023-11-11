fc5d010a90b0b64b07e544a8b0a5d42bc9232bb3
rafec <rafael.brizuena@gmail.com>
2023-11-10 21:17:49 -0400

    feat: now the current theme is saved on local storage, and loaded from there

--- dashboard-react-syncfusion/src/contexts/ContextProvider.js
+++ dashboard-react-syncfusion/src/contexts/ContextProvider.js
@@ -14,7 +14,9 @@ export const ContextProvider = ({ children }) => {
 	const [isClicked, setIsClicked] = useState(initialState);
 	const [screenSize, setScreenSize] = useState(undefined);
 	const [currentColor, setCurrentColor] = useState("#03C9D7");
-	const [currentMode, setCurrentMode] = useState("Light");
+	const [currentMode, setCurrentMode] = useState(
+		localStorage.getItem("themeMode")
+	);
 	const [themeSettings, setThemeSettings] = useState(false);
 
 	const setMode = (e) => {
