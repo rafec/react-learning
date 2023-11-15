637bcf7d916b073eb4b108189b79041ac00a1d95
rafec <rafael.brizuena@gmail.com>
2023-11-15 16:31:32 -0400

    feat: added plane 3d model

--- 3d-portfolio/src/models/Bird.jsx
+++ 3d-portfolio/src/models/Bird.jsx
@@ -4,6 +4,7 @@ import { useGLTF } from '@react-three/drei'
 
 const Bird = () => {
   const { scene, animations } = useGLTF(birdScene)
+  
   return (
     <mesh position={[1, 1, 1]} scale={[0.003, 0.003, 0.003]}>
       <primitive object={scene} />
