9c512fe2d254574e7de9094d8e2aa385760fa8b8
rafec <rafael.brizuena@gmail.com>
2023-11-15 16:25:04 -0400

    feat: added bird 3d model

--- 3d-portfolio/src/pages/Home.jsx
+++ 3d-portfolio/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import Loader from '../components/Loader'
 
 import Island from '../models/Island'
 import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -33,12 +35,14 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
 
+          <Bird />
           <Sky />
           <Island
           position={islandPosition}
           scale={islandScale}
           rotation={islandRotation}
           />
+          <Plane />
         </Suspense>
       </Canvas>
     </section>
