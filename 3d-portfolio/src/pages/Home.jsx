a2a0b82eb1c0bbd78eccdde67b86b6ce7053e4c3
rafec <rafael.brizuena@gmail.com>
2023-11-15 16:17:46 -0400

    feat: added sky with clouds

--- 3d-portfolio/src/pages/Home.jsx
+++ 3d-portfolio/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
 import Island from '../models/Island'
+import Sky from '../models/Sky'
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -32,6 +33,7 @@ const Home = () => {
           <ambientLight intensity={0.5} />
           <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
 
+          <Sky />
           <Island
           position={islandPosition}
           scale={islandScale}
