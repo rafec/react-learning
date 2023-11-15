e20ad6a863f15d440daa0b200a65fdea9294788f
rafec <rafael.brizuena@gmail.com>
2023-11-15 15:35:50 -0400

    feat: added assets and created first 3d element in the homepage

--- 3d-portfolio/src/pages/Home.jsx
+++ 3d-portfolio/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
+import Island from '../models/Island'
+
 const Home = () => {
   return (
     <section className='w-full h-screen relative'>
@@ -10,7 +12,13 @@ const Home = () => {
       className='w-full h-screen bg-transparent'
       camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
 
+          <Island />
         </Suspense>
       </Canvas>
     </section>
