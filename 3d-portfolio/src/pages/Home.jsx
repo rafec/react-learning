81ce30113557a03c2cb0822d4857594f4bef383d
rafec <rafael.brizuena@gmail.com>
2023-11-25 20:51:01 -0400

    feat: adding animation to the plane blade and fixing setCurrentStage missing declaration

--- 3d-portfolio/src/pages/Home.jsx
+++ 3d-portfolio/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import Plane from '../models/Plane'
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false)
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -60,6 +61,7 @@ const Home = () => {
           rotation={islandRotation}
           isRotating={isRotating}
           setIsRotating={setIsRotating}
+          setCurrentStage={setCurrentStage}
           />
           <Plane
             isRotating={isRotating}
