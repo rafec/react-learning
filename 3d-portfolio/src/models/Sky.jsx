0f3cb0f2180c032851402e94f0d6aa722f7cb003
rafec <rafael.brizuena@gmail.com>
2023-11-25 20:59:55 -0400

    feat: adding movement to the sky

--- 3d-portfolio/src/models/Sky.jsx
+++ 3d-portfolio/src/models/Sky.jsx
@@ -2,14 +2,24 @@ import { useGLTF } from '@react-three/drei'
 import React from 'react'
 
 import skyScene from '../assets/3d/sky.glb'
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 
-const Sky = () => {
-    const sky = useGLTF(skyScene)
+const Sky = ({ isRotating }) => {
+  const sky = useGLTF(skyScene)
+  const skyRef = useRef();
+
+  useFrame((_, delta) => {
+    if(isRotating) {
+      skyRef.current.rotation.y +=0.25 * delta;
+    }
+  })
+
   return (
-    <mesh>
+    <mesh ref={skyRef}>
         <primitive object={sky.scene} />
     </mesh>
   )
 }
 
-export default Sky
+export default Sky;
--- 3d-portfolio/src/pages/Home.jsx
+++ 3d-portfolio/src/pages/Home.jsx
@@ -54,7 +54,7 @@ const Home = () => {
           <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
 
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island
           position={islandPosition}
           scale={islandScale}
