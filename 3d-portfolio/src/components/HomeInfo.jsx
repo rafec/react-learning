c0e3420701abda276422f0d65920e917e349f41e
rafec <rafael.brizuena@gmail.com>
2023-12-01 20:27:08 -0800

    feat: created cards for popups with text and links, and styles too

--- 3d-portfolio/src/components/HomeInfo.jsx
+++ 3d-portfolio/src/components/HomeInfo.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
 
-const infoBox = ({ text, link, btnText }) => <div>{text}</div>;
+const InfoBox = ({ text, link, btnText }) => (
+	<div className="info-box">
+		<p className="font-medium sm:text-xl text-center">{text}</p>
+		<Link to={link} className="neo-brutalism-white neo-btn">
+			{btnText}
+			<img src={arrow} className="w-4 h-4 object-contain" />
+		</Link>
+	</div>
+);
 
 const renderContent = {
 	1: (
@@ -9,9 +19,27 @@ const renderContent = {
 			<br />A Software Engineer from Brazil
 		</h1>
 	),
-	2: <h1>2</h1>,
-	3: <h1>3</h1>,
-	4: <h1>4</h1>,
+	2: (
+		<InfoBox
+			text="Worked with many companies and picked many skills along the way"
+			link="/about"
+			btnText="Learn more"
+		/>
+	),
+	3: (
+		<InfoBox
+			text="Led multiple projects to success over the years. Curious about the impact?"
+			link="/projects"
+			btnText="Visit my portfolio"
+		/>
+	),
+	4: (
+		<InfoBox
+			text="Need a project done? Or looking for a dev? I'm just a few keystrokes away!"
+			link="/contact"
+			btnText="Let's talk"
+		/>
+	),
 };
 
 const HomeInfo = ({ currentStage }) => {
