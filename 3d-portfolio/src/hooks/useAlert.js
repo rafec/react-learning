8f4d5b76d5e9b55324489f9d3dffb845d3c6ba98
rafec <rafael.brizuena@gmail.com>
2023-12-02 16:38:54 -0800

    feat: finished alert functionality and style

--- 3d-portfolio/src/hooks/useAlert.js
+++ 3d-portfolio/src/hooks/useAlert.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const useAlert = () => {
 	const [alert, setAlert] = useState({ show: false, text: "", type: "danger" });
 
-	const showAlert = (text, type = "danger") =>
+	const showAlert = ({ text, type = "danger" }) =>
 		setAlert({
 			show: true,
 			text,
