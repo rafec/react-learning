f8c3c55262c7ee96f9bfc752b408970d4a1619de
rafec <rafael.brizuena@gmail.com>
2023-12-02 18:10:10 -0800

    feat: created first layout for Projects page

--- 3d-portfolio/src/pages/Projects.jsx
+++ 3d-portfolio/src/pages/Projects.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+	return (
+		<section className="max-container">
+			<h1 className="head-text">
+				My{" "}
+				<span className="blue-gradient_text font-semibold drop-shadow">
+					Projects
+				</span>
+			</h1>
 
-export default Projects
+			<div className="mt-5 flex flex-col gap-3 text-slate-500">
+				<p>
+					I've embarked on numerous projects throughout the years, but these are
+					the ones I hold closest to my heart. Many of them are open-source, so
+					if you come across something that piques your interest, feel free to
+					explore the codebase and contribute your ideas for further
+					enhancements. Your collaboration is highly valued!
+				</p>
+			</div>
+		</section>
+	);
+};
+
+export default Projects;
