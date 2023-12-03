f034d5650c5c84feabc93b0be9ae9e5655fc96ed
rafec <rafael.brizuena@gmail.com>
2023-12-02 17:55:49 -0800

    feat: created CTA component to allow contact page link in the ending of About page

--- 3d-portfolio/src/pages/About.jsx
+++ 3d-portfolio/src/pages/About.jsx
@@ -5,6 +5,7 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 
 import { skills, experiences } from "../constants";
+import CTA from "../components/CTA";
 
 const About = () => {
 	return (
@@ -100,6 +101,10 @@ const About = () => {
 					</VerticalTimeline>
 				</div>
 			</div>
+
+			<hr className="border-slate-200" />
+
+			<CTA />
 		</section>
 	);
 };
