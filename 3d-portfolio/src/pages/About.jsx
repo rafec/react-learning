e3e8f30c9a07d7f69e42cab14e608ed143031aed
rafec <rafael.brizuena@gmail.com>
2023-12-02 16:52:59 -0800

    feat: created first layout for about page, and created generic file with some ficticious informations

--- 3d-portfolio/src/pages/About.jsx
+++ 3d-portfolio/src/pages/About.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
+import { skills } from "../constants";
+
 const About = () => {
-	return <div>About</div>;
+	return (
+		<section className="max-container">
+			<h1 className="head-text">
+				Hello, I'm{" "}
+				<span className="blue-gradient_text font-semibold drop-shadow">
+					Rafael
+				</span>
+			</h1>
+
+			<div className="mt-5 flex flex-col gap-3 text-slate-500">
+				<p>
+					Software Engineer based in Brazil, specializing in technical education
+					through hands-on learning and building applications.
+				</p>
+			</div>
+
+			<div className="py-10 flex flex-col">
+				<h3 className="subhead-text">My Skills</h3>
+				<div className="mt-16 flex flex-wrap gap-12">
+					{skills.map((skill) => (
+						<div>
+							<div>
+								<img
+									src={skill.imageUrl}
+									alt={skill.name}
+									className="w-1/2 h-1/2 object-contain"
+								/>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default About;
