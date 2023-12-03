442d3db90739c04c444f2fbf6709e30900101132
rafec <rafael.brizuena@gmail.com>
2023-12-02 16:06:12 -0800

    feat: added animations to make the fox run, walk or idle depending on user's behaviours

--- 3d-portfolio/src/pages/Contact.jsx
+++ 3d-portfolio/src/pages/Contact.jsx
@@ -10,6 +10,7 @@ const Contact = () => {
 	const formRef = useRef(null);
 	const [form, setForm] = useState({ name: "", email: "", message: "" });
 	const [isLoading, setIsLoading] = useState(false);
+	const [currentAnimation, setCurrentAnimation] = useState("idle");
 
 	const handleChange = (e) => {
 		setForm({ ...form, [e.target.name]: e.target.value });
@@ -18,6 +19,7 @@ const Contact = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		setIsLoading(true);
+		setCurrentAnimation("hit");
 
 		emailjs
 			.send(
@@ -41,13 +43,14 @@ const Contact = () => {
 			})
 			.catch((error) => {
 				setIsLoading(false);
+				setCurrentAnimation("idle");
 				console.log(error);
 				// TODO: SHOW ERROR MESSAGE
 			});
 	};
 
-	const handleFocus = () => {};
-	const handleBlur = () => {};
+	const handleFocus = () => setCurrentAnimation("walk");
+	const handleBlur = () => setCurrentAnimation("idle");
 
 	return (
 		<section className="relative flex lg:flex-row flex-col max-container">
@@ -125,6 +128,7 @@ const Contact = () => {
 					<ambientLight intensity={0.5} />
 					<Suspense fallback={<Loader />}>
 						<Fox
+							currentAnimation={currentAnimation}
 							position={[0.5, 0.35, 0]}
 							rotation={[12.6, -0.6, 0]}
 							scale={[0.5, 0.5, 0.5]}
